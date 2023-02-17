e63df6ed7607b3debc48d288de1187268d93a154
Jane <orihijeoma@gmail.com>
2023-02-16 20:52:02 -0800

    56

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* include is for introducing the library
+ * main for opening the program
+ * printf for printing
+ * return (0) success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);	
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+include <stdio.h>
+/* include is fore the library
+ * main is for the opening
+ * returnn 0 success
+ *
+ */
+int main(void)
+{
+	printf("Size of a char: %2d byte(s)\n", sizeof(char));
+	printf("Size of an int: %2d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %2d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %2d byte(s)\n", sizeof(long double));
+	printf("Size of an float: %2d byte(s)\n", sizeof(float));
+	return (0);
+}
