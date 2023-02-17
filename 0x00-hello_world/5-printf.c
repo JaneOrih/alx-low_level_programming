a8dfc1a1de57d7c34a26abb1345e60ef639c7552
Jane <orihijeoma@gmail.com>
2023-02-16 21:15:23 -0800

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /*
  * include is for introducing the library
- * main - for entry or opening to the program
+ * main - entry or opening to the program
  * printf for printing
  * return (0) success
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);	
+	return (0);
 }
