db2a47b5260ed1ac2ac3b9354fcb621794c2b2ca
Jane <orihijeoma@gmail.com>
2023-02-17 12:46:35 -0800

    67

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,12 +4,13 @@
  * main - main function
  * Return: always 0
  */
+
 int main(void)
 {
 	int c;
 
-	for (c = 0; c <= 9; c++)
-		putchar('0' + c);
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
 	putchar('\n');
-	return (0);
+	return(0);
 }
