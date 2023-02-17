cc7289640132a627ae9ba9c8ba6318b7f296258c
Jane <orihijeoma@gmail.com>
2023-02-16 19:26:31 -0800

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* main opens the programme
+ * return shows std output
+ * include denotes library
+ *
+ * puts for printing str and strtung the next in a new line
+ * char for denoting character
+ */
+int main(void)
+{
+	char the_str[100]= "Programming is like building a multilingual puzzle";
+	puts(the_str);
+	return (0);
+}
