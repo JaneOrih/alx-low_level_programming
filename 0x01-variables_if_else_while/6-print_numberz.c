e2c81ac525c58f10ee8ca17c1d08cc49117ba972
Jane <orihijeoma@gmail.com>
2023-02-17 12:33:23 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: always 0
+ */
+int main(void)	
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+		putchar('0' + c);
+	putchar('\n');
+	return (0);
+}
