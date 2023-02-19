f07914e2829c9a5f6eb248116f615b88340b36ca
Jane <orihijeoma@gmail.com>
2023-02-19 06:24:00 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-/* main opens the programme
- * Return : always 0
- * include denotes library
- *
- * puts for printing str and strtung the next in a new line
- * char for denoting character
+/**
+ * main - Entry point
+ * Return: always 0
  */
+
 int main(void)
 {
 	char the_str[100]= "\"Programming is like building a multilingual puzzle";
