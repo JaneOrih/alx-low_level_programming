30b49e1911abb7a326bee7d1abc1da73d8cd0141
Jane <orihijeoma@gmail.com>
2023-02-23 14:36:35 -0800

    digit and multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - function for multiplication
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: not always 0
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
