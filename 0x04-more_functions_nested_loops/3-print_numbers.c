72cfd79d7e8c90595398f9552bf3de52bbc9c732
Jane <orihijeoma@gmail.com>
2023-02-23 19:44:11 -0800

    003

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function for printing numbers
+ *
+ * Return: not in this function 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i =48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
