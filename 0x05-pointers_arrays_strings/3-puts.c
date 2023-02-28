672d07b3a4efebeab82d38f7a3a997ad48674a87
Jane <orihijeoma@gmail.com>
2023-02-27 17:29:07 -0800

    I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts - function for string printing
+ * @str: parameter
+ * Return: returning void
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
