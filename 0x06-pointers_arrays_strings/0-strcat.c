b374a96a7f90cabd588ee95898dc112d61630b99
Jane <orihijeoma@gmail.com>
2023-03-02 05:00:37 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,8 +2,8 @@
 #include <string.h>
 /**
  * *_strcat - function appends the src string to the dest string
- * *dest: parameter
- * *src: prameter
+ * @dest: parameter
+ * @src: prameter
  * Return: type char
  */
 char *_strcat(char *dest, char *src)
