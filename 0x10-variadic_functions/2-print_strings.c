082ef93745452576f6c68c412749f152ab967711
Jane <orihijeoma@gmail.com>
2023-03-23 16:24:51 -0700

    printing strings and integers w/variadic functions

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_strings -  function that prints strings
+ * @separator: string to be printed
+ * @n: number of strings passed
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+
+	va_list args;
+
+	va_start(args, n);
+	if (separator == NULL)
+	{
+		return;
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			str = va_arg(args, char *);
+			if (str == NULL)
+			{
+				printf("nil\n");
+				break;
+			}
+			else
+			{
+				printf("%s", str);
+				if (i + 1 != n)
+				{
+					printf("%s", separator);
+				}
+			}
+
+		}
+	}
+	printf("\n");
+}
