93e9b8d947379b58d87d7c4b927405bc2305380a
Jane <orihijeoma@gmail.com>
2023-03-23 19:28:11 -0700

    value of variable

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,7 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			str = va_arg(args, char *);
 			if (str == NULL)
 			{
-				printf("nil\n");
+				printf("(nil)");
 				break;
 			}
 			else
