1609918030a1282d95b1ae205a76c03cf71823ac
Jane Orih <orihijeoma@gmail.com>
2023-03-31 12:37:46 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,13 +11,13 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	if (h->str == NULL)
+	while (h)
 	{
-		printf("[0] (nil)\n");
-	}
-	while (h != NULL)
-	{
-		if (h->str != NULL)
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
 		}
