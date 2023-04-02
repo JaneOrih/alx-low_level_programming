260e1ea1954284a5cceb4e8fc724cfdee715e7d8
Jane Orih <orihijeoma@gmail.com>
2023-04-02 02:34:43 -0700

    multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,10 +9,17 @@
  */
 int main(int argc, char *argv[])
 {
-	int mul;
+	int num1, num2, mul;
 
-	mul = atoi(argv[1]) * atoi(argv[2]);
-	(void) argc;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mul = num1 * num2;
 	printf("%d\n", mul);
 	return (0);
 }
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <ctype.h> 
+
+/**
+ * is_digit - function to chek if input is a digit
+ * @x: input value/ string
+ * Return: integer
+ */
+int is_digit(int argc, char *x)
+{
+	int j;
+
+	for (j = 0; j < argc; j++)
+	{
+		if (isdigit(arg[v]))
+	}
+}
+
+/**
+ * main - maun function
+ * @argc: argument count
+ * @argv: argument vector
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+	}
+	else
+	{
+		for (i = 0; i < argc; i++)
+		{
+			if (argv[i] != 
+		}
+	}
+}
